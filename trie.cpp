2ce1f0209d7a9d07370a316385778b9b8b34480b
astha singh <43131592+ashu-111@users.noreply.github.com>
2019-04-13 11:12:34 -0700

    Update trie.cpp

--- trie.cpp
+++ trie.cpp
@@ -1,39 +1,100 @@
-#include<bits/stdc++.h>
-using namespace std;
-struct TrieNode{
-    struct TrieNode *children[26];
-    bool endOfWord;
+
+
+C++ program for Inserting and Searching in a Trie.
+
+Solution: We make a trie node as root. For both the operation we use a current pointer starting from root.
+        To insert into a trie, we keep on traversing if the node is already present. 
+            If not, then we make the node and traverse forward. 
+            We mark the last character node with isEndOfWord=true
+        To search into a trie, we keep on checking if the current pointer is not NULL.
+        If at at any point it is NULL, then we return false.
+        If not, then we traverse till the end and check if isEndOfWord==true.
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std; 
+#define ALPHABET_SIZE 26
+
+struct TrieNode {
+    struct TrieNode* children[ALPHABET_SIZE];
+    bool isEndOfWord;
+    TrieNode(){
+        for(int i=0;i<ALPHABET_SIZE;i++)
+            this->children[i]=NULL;
+        this->isEndOfWord=NULL;
+    }
 };
 
-struct TrieNode *getNode(){
-    struct TrieNode *tmp = new TrieNode;
-    tmp->endOfWord = false;
-    for (int i = 0; i < 26; i++)
-        tmp->children[i] = NULL;
-    return tmp;                    
+typedef struct TrieNode TrieNode;
+
+void insertIntoTrie(TrieNode *root, string s){
+    int n=s.size();
+    
+    // Taking a TrieNode which we will use for traversing the Trie.
+    TrieNode *current = root;
+    
+    // Iterate over all the characters of the string to make nodes.
+    for(int i=0;i<n;i++){
+        int index = s[i]-'a';
+        
+        // Check for the children index. Make a new node, if one doesn't exist already.
+        if(current->children[index]==NULL)
+            current->children[index] = new TrieNode;
+        
+        // Keep moving forward...
+        current = current->children[index];
+    }
+    current->isEndOfWord = true;
 }
 
-void insert(struct TrieNode *root, string word)
-{
-    struct TrieNode *tmp = root;
-     for (int i = 0; i < word.length(); i++)
-     {
-        int index = word[i]-'a';
-        if(!tmp->children[index])
-            tmp->children[index] = getNode();
-        tmp = tmp->children[index];            
-     }
-     tmp->endOfWord = true;
-};
-bool search(struct TrieNode *root,string word)
-{
-   struct TrieNode *tmp=root;
-   for(int i=0; i<word.length();i++)
-   {
-       int index=word[i]-'a';
-       if(!tmp->children[index])
-          return false;
-        tmp=tmp->children[index] ; 
-   }
-   return (tmp != NULL && tmp->endOfWord);
+bool searchIntoTrie(TrieNode *root, string s){
+    int n=s.size();
+    
+    // Taking a TrieNode which we will use for traversing the Trie.
+    TrieNode *current = root;
+    
+    // Iterate over all the characters of the string to searchfor TrieNodes.
+    for(int i=0;i<n;i++){
+        int index = s[i]-'a';
+        
+        // Check if at any point if a node doesn't exist, then the string doesn't exist in trie.
+        if(current->children[index]==NULL)
+            return 0;
+        
+        // Keep moving forward...
+        current = current->children[index];
+    }
+    
+    // If isEndOfWord is set and the last node is set, then the string is present.
+    if(current!=NULL && current->isEndOfWord)
+        return 1;
+    else
+        return 0;
 }
+
+
+int main() 
+{
+    // Making a root Trie node.
+    TrieNode *root = new TrieNode;
+    insertIntoTrie(root, "harshit");
+    insertIntoTrie(root, "harsh");
+    
+    if (searchIntoTrie(root, "harshit"))
+        cout<<"harshit is present in Trie"<<endl;
+    else
+        cout<<"harshit is NOT present in Trie"<<endl;
+        
+    if(searchIntoTrie(root, "gupta"))
+        cout<<"gupta is present in Trie"<<endl;
+    else
+        cout<<"gupta is NOT present in Trie"<<endl;
+        
+    if (searchIntoTrie(root, "harsh"))
+        cout<<"harsh is present in Trie"<<endl;
+    else
+        cout<<"harsh is NOT present in Trie"<<endl;
+        
+	return 0; 
+} 
